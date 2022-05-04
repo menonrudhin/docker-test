ab0b83e07df536bf90d763b3274c050b1b32b2f8
Rudhin Menon <menonrudhin@gmail.com>
2022-05-03 22:11:45 -0400

    zookeeper and kafka is up but client did not connect, possibly broker issue

--- src/main/java/test/kafka/kafkaclient/KafkaclientApplication.java
+++ src/main/java/test/kafka/kafkaclient/KafkaclientApplication.java
@@ -5,12 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
 
 @SpringBootApplication
 public class KafkaclientApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Inside Main");
+		System.out.println("Inside Main"); // 192.168.0.3:29092
 		SpringApplication.run(KafkaclientApplication.class, args);
 		System.out.println("Exit NewTopic");
 	}
