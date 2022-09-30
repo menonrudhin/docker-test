ed81d6b3f8a333acd27e7bbf70b7e16002a9f226
Rudhin Menon <menonrudhin@gmail.com>
2022-09-29 22:42:25 -0400

    random try, kafka client still doesn't connect and nor does it recognize application.properties file

--- src/main/java/test/kafka/kafkaclient/KafkaclientApplication.java
+++ src/main/java/test/kafka/kafkaclient/KafkaclientApplication.java
@@ -6,12 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:application.properties")
 public class KafkaclientApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Inside Main"); // 192.168.0.3:29092
+		System.out.println("Kafka broker is : " + System.getProperty("bootstrap.servers")); 
 		SpringApplication.run(KafkaclientApplication.class, args);
 		System.out.println("Exit NewTopic");
 	}
