e208f77195eef9bec9028cd55aac1a9dea2e2e06
Rudhin Menon <menonrudhin@gmail.com>
2022-11-23 21:51:47 -0500

    this commit worked kafka client listed the message produced manually in kafka server shell

--- src/main/java/test/kafka/kafkaclient/KafkaclientApplication.java
+++ src/main/java/test/kafka/kafkaclient/KafkaclientApplication.java
@@ -1,22 +1,31 @@
 package test.kafka.kafkaclient;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.config.KafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaAdmin;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
-@PropertySource(value = "classpath:application.properties")
+@EnableKafka
 public class KafkaclientApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Inside Main"); // 192.168.0.3:29092
-		System.out.println("Kafka broker is : " + System.getProperty("bootstrap.servers")); 
 		SpringApplication.run(KafkaclientApplication.class, args);
-		System.out.println("Exit NewTopic");
+		System.out.println("Exit Main");
 	}
 
 	@Bean
@@ -28,5 +37,30 @@ public class KafkaclientApplication {
 				.build();
 	}
 
+	@Bean
+	KafkaListenerContainerFactory<ConcurrentMessageListenerContainer<String, String>>
+	kafkaListenerContainerFactory() {
+		ConcurrentKafkaListenerContainerFactory<String, String> factory =
+				new ConcurrentKafkaListenerContainerFactory<>();
+		factory.setConsumerFactory(consumerFactory());
+		factory.setConcurrency(3);
+		factory.getContainerProperties().setPollTimeout(3000);
+		return factory;
+	}
 
+	@Bean
+	public ConsumerFactory<String, String> consumerFactory() {
+		return new DefaultKafkaConsumerFactory<>(consumerProps());
+	}
+
+	private Map<String, Object> consumerProps() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "broker:29092");
+		props.put(ConsumerConfig.GROUP_ID_CONFIG, "group");
+		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+		props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+		// ...
+		return props;
+	}
 }
--- src/main/java/test/kafka/kafkaclient/MessageConsumer.java
+++ src/main/java/test/kafka/kafkaclient/MessageConsumer.java
@@ -1,12 +1,17 @@
 package test.kafka.kafkaclient;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageConsumer {
+    public MessageConsumer() {
+        System.out.println("********* MessageConsumer constructor ******");
+    }
+
     @KafkaListener(id="myid" , topics="topic1")
-    public void listen(String in) {
-        System.out.println(in);
+    public void listen(ConsumerRecord<String, String> record) {
+        System.out.println("Record key = " + record.key() + " and value = " + record.value());
     }
 }
